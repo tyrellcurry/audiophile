010e7c3a4a52e61620ba3cdb6b3695e4989dbcda
Tyrell C <tyrellrcurry@gmail.com>
2023-04-11 17:49:31 -0700

    Rendering users on page from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,34 @@
-import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
 
-function App() {
-    const requestOptions = {
-        method: "GET",
-    };
+function Home() {
+  interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
 
-    fetch("http://localhost:8000/users", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.log("error", error));
+  const [users, setUsers] = useState<User[]>([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:8000/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <Counter />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <span>
-                    <span>Learn </span>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org/"
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        React
-                    </a>
-                    <span>, </span>
-                    <a
-                        className="App-link"
-                        href="https://redux.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        Redux
-                    </a>
-                    <span>, </span>
-                    <a
-                        className="App-link"
-                        href="https://redux-toolkit.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        Redux Toolkit
-                    </a>
-                    ,<span> and </span>
-                    <a
-                        className="App-link"
-                        href="https://react-redux.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        React Redux
-                    </a>
-                </span>
-            </header>
+  return (
+    <div>
+      <h1>Hello World</h1>
+      {users.map(user => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
-export default App;
+export default Home;
